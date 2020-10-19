ccc5a536af3f195b3dad3bf2ca73695e3686c8b9
Jesse-Lucas1996 <61496789+Jesse-Lucas1996@users.noreply.github.com>
2020-10-19 14:37:06 +0800

    Add files via upload

---
+++ MediaPlayerForm/MediaPlayerForm.Designer.cs
@@ -0,0 +1,177 @@
+﻿using WMPLib;
+
+namespace MediaPlayerForm
+{
+    partial class AudioPlayerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AudioPlayerForm));
+            this.buttonFirst = new System.Windows.Forms.Button();
+            this.buttonLast = new System.Windows.Forms.Button();
+            this.buttonNext = new System.Windows.Forms.Button();
+            this.buttonPrevious = new System.Windows.Forms.Button();
+            this.textBoxCurrentSong = new System.Windows.Forms.TextBox();
+            this.textBoxError = new System.Windows.Forms.TextBox();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.buttonStop = new System.Windows.Forms.Button();
+            this.ErrorMsg = new System.Windows.Forms.StatusStrip();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonFirst
+            // 
+            this.buttonFirst.Location = new System.Drawing.Point(32, 45);
+            this.buttonFirst.Name = "buttonFirst";
+            this.buttonFirst.Size = new System.Drawing.Size(75, 23);
+            this.buttonFirst.TabIndex = 0;
+            this.buttonFirst.Text = "Play";
+            this.buttonFirst.UseVisualStyleBackColor = true;
+            this.buttonFirst.Click += new System.EventHandler(this.ButtonFirst_Click);
+            // 
+            // buttonLast
+            // 
+            this.buttonLast.Location = new System.Drawing.Point(475, 45);
+            this.buttonLast.Name = "buttonLast";
+            this.buttonLast.Size = new System.Drawing.Size(75, 23);
+            this.buttonLast.TabIndex = 1;
+            this.buttonLast.Text = "Last";
+            this.buttonLast.UseVisualStyleBackColor = true;
+            this.buttonLast.Click += new System.EventHandler(this.ButtonLast_Click);
+            // 
+            // buttonNext
+            // 
+            this.buttonNext.Location = new System.Drawing.Point(173, 45);
+            this.buttonNext.Name = "buttonNext";
+            this.buttonNext.Size = new System.Drawing.Size(75, 23);
+            this.buttonNext.TabIndex = 2;
+            this.buttonNext.Text = "Next";
+            this.buttonNext.UseVisualStyleBackColor = true;
+            this.buttonNext.Click += new System.EventHandler(this.ButtonNext_Click);
+            // 
+            // buttonPrevious
+            // 
+            this.buttonPrevious.Location = new System.Drawing.Point(323, 45);
+            this.buttonPrevious.Name = "buttonPrevious";
+            this.buttonPrevious.Size = new System.Drawing.Size(75, 23);
+            this.buttonPrevious.TabIndex = 3;
+            this.buttonPrevious.Text = "Previous";
+            this.buttonPrevious.UseVisualStyleBackColor = true;
+            this.buttonPrevious.Click += new System.EventHandler(this.ButtonPrevious_Click);
+            // 
+            // textBoxCurrentSong
+            // 
+            this.textBoxCurrentSong.Location = new System.Drawing.Point(32, 90);
+            this.textBoxCurrentSong.Name = "textBoxCurrentSong";
+            this.textBoxCurrentSong.Size = new System.Drawing.Size(216, 20);
+            this.textBoxCurrentSong.TabIndex = 4;
+            // 
+            // textBoxError
+            // 
+            this.textBoxError.Location = new System.Drawing.Point(41, 430);
+            this.textBoxError.Name = "textBoxError";
+            this.textBoxError.Size = new System.Drawing.Size(238, 20);
+            this.textBoxError.TabIndex = 5;
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(475, 90);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 6;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.ButtonAdd_Click);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 131);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(540, 219);
+            this.axWindowsMediaPlayer1.TabIndex = 7;
+
+            // 
+            // buttonStop
+            // 
+            this.buttonStop.Location = new System.Drawing.Point(323, 90);
+            this.buttonStop.Name = "buttonStop";
+            this.buttonStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStop.TabIndex = 8;
+            this.buttonStop.Text = "Stop";
+            this.buttonStop.UseVisualStyleBackColor = true;
+            this.buttonStop.Click += new System.EventHandler(this.ButtonStop_Click);
+            // 
+            // ErrorMsg
+            // 
+            this.ErrorMsg.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.ErrorMsg.Location = new System.Drawing.Point(0, 552);
+            this.ErrorMsg.Name = "ErrorMsg";
+            this.ErrorMsg.Size = new System.Drawing.Size(1437, 22);
+            this.ErrorMsg.TabIndex = 9;
+            this.ErrorMsg.Text = "statusStrip1";
+            // 
+            // AudioPlayerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1437, 574);
+            this.Controls.Add(this.ErrorMsg);
+            this.Controls.Add(this.buttonStop);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.textBoxError);
+            this.Controls.Add(this.textBoxCurrentSong);
+            this.Controls.Add(this.buttonPrevious);
+            this.Controls.Add(this.buttonNext);
+            this.Controls.Add(this.buttonLast);
+            this.Controls.Add(this.buttonFirst);
+            this.Name = "AudioPlayerForm";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonFirst;
+        private System.Windows.Forms.Button buttonLast;
+        private System.Windows.Forms.Button buttonNext;
+        private System.Windows.Forms.Button buttonPrevious;
+        private System.Windows.Forms.TextBox textBoxCurrentSong;
+        private System.Windows.Forms.TextBox textBoxError;
+        private System.Windows.Forms.Button buttonAdd;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button buttonStop;
+        private System.Windows.Forms.StatusStrip ErrorMsg;
+    }
+}
+
---
+++ MediaPlayerForm/MediaPlayerForm.cs
@@ -0,0 +1,142 @@
+﻿using AxWMPLib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WMPLib;
+
+// Jesse Lucas
+// P274063
+//24/07/2020
+// Question 1 – You need to make a Music Player for all staff to use. You need to 
+//create a program that plays audio tracks, it must have the ability to go to the start,
+//go back one, go forward one and skip to the end.It must have a GUI and must use
+//doubly linked LinkedLists.
+
+namespace MediaPlayerForm
+{
+    public partial class AudioPlayerForm : Form
+    {
+        public AudioPlayerForm()
+        {
+            InitializeComponent();
+        }
+        // creating the doubly linked LinkedList
+        LinkedList<string> SongLinkedList = new LinkedList<string>();
+
+
+        // using the windows media player URl function 
+        // we get the first value and plays using playSound()
+        private void ButtonFirst_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string songPlaying = axWindowsMediaPlayer1.URL;
+                PlaySound(SongLinkedList.First.Value);
+                DisplaySongs(SongLinkedList.First.Value);
+            }
+            catch
+            {
+                textBoxError.Text = "No songs loaded";
+            }
+        }
+        // using the windows mediaplayer URL function and the playSound we can get the next value and plays
+        private void ButtonNext_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string songPlaying = axWindowsMediaPlayer1.URL;
+                PlaySound(SongLinkedList.Find(songPlaying).Next.Value);
+                DisplaySongs(SongLinkedList.Find(songPlaying).Next.Value);
+             }
+            catch
+            {
+                textBoxError.Text = "Error can not play next";
+            }            
+        }
+
+        // using the windows media player URL and the playSound method we can get the previous value and play
+        private void ButtonPrevious_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string songPlaying = axWindowsMediaPlayer1.URL;
+                PlaySound(SongLinkedList.Find(songPlaying).Previous.Value);
+                DisplaySongs(SongLinkedList.Find(songPlaying).Previous.Value);
+            }
+            catch
+            {
+                textBoxError.Text = "Error can not play previous";
+            }
+        }
+
+        // using the windows media player URl and the play sound method we can get the last value and play
+    private void ButtonLast_Click(object sender, EventArgs e)
+        {
+            string songPlaying = axWindowsMediaPlayer1.URL;
+            PlaySound(SongLinkedList.Last.Value);
+            DisplaySongs(SongLinkedList.Last.Value);
+        }
+
+        // here we open a file dialog and we can add the songs to the mediaplayer which adds to our linked LinkedList
+        private void ButtonAdd_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.RestoreDirectory = true;
+            openFileDialog.Multiselect = true;
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;                
+                SongLinkedList.AddLast(filePath.ToString());           
+            }
+          
+        }
+        // here we just stop the music
+            private void ButtonStop_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                axWindowsMediaPlayer1.Ctlcontrols.stop();
+            }
+            catch
+            {
+                ErrorMsg.Text = "Can't stop won't stop";
+            }
+        }
+
+        // here we use the windows media player URL with string playsong then we simply play the music
+        public void PlaySound(string playsong)
+        {            
+            try
+            {
+                axWindowsMediaPlayer1.URL = playsong;
+                axWindowsMediaPlayer1.Ctlcontrols.play();
+            }
+            catch
+            {
+                ErrorMsg.Text = "Can not play";
+            }
+            }
+
+        // Displaying songs here we split the string pathnae and file name so it just displays the song name and not the whole path name
+        public void DisplaySongs(string title)
+        {
+            string songPlaying = axWindowsMediaPlayer1.URL;
+            PlaySound(SongLinkedList.Find(songPlaying).Value);
+            string[] pathname = title.Split('\\');
+            string[] filename = pathname.Last().Split('.');
+            string songtitle = filename.First().ToString();
+            textBoxCurrentSong.Text = songtitle;
+        }
+
+       
+    }
+    }
+
